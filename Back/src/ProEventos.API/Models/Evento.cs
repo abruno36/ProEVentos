f883465ccbf1436f82e59eb16010823c69900cb0
AntonioBruno <abruno36@gmail.com>
2021-06-05 20:41:48 -0300

    introdução do EF Core

--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -10,12 +10,12 @@ namespace ProEventos.API.Models
         public DateTime?  DataEvento { get;  set; }
         public string Tema { get;  set; }
         public int QtdPessoas { get;  set; }
-        public string Lote { get; set; }
+        //public string Lote { get; set; }
         public string ImagemURL { get;  set; }
         public string Telefone { get; private set; }
         public string Email { get; private set; }
-        public List<Lote> Lotes { get; set; }
-        public List<RedeSocial> RedesSociais { get; set; }
-        public List<PalestranteEvento> PalestrantesEventos { get; private set; }
+        //public List<Lote> Lotes { get; set; }
+        //public List<RedeSocial> RedesSociais { get; set; }
+        //public List<PalestranteEvento> PalestrantesEventos { get; private set; }
     }
 }
