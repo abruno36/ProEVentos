d5eaa6a6331fa89d1029f7d417b1bdca90e36152
AntonioBruno <abruno36@gmail.com>
2021-06-11 17:13:36 -0300

    Atualizações Gerais - Front - 110620211713

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -23,11 +23,17 @@ import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { TituloComponent } from './shared/titulo/titulo.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 
 
 defineLocale('pt-br', ptBrLocale);
@@ -42,7 +48,12 @@ defineLocale('pt-br', ptBrLocale);
     PerfilComponent,
     DateTimeFormatPipe,
     NavComponent,
-    TituloComponent
+    TituloComponent,
+    EventoDetalheComponent,
+    EventoListaComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
    ],
    schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
